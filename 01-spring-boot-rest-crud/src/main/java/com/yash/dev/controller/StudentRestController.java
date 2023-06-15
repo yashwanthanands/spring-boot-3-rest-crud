3e7eb6a0d6f5d368cce00e85b4aacbf651c69aa8
Yashwanth Anand S <yashwanthanands@outlook.com>
2023-06-15 14:45:22 +0530

    Implemented ExceptionHandler for endpoint

--- 01-spring-boot-rest-crud/src/main/java/com/yash/dev/controller/StudentRestController.java
+++ 01-spring-boot-rest-crud/src/main/java/com/yash/dev/controller/StudentRestController.java
@@ -1,9 +1,14 @@
 package com.yash.dev.controller;
 
 import com.yash.dev.entity.Student;
+import com.yash.dev.exception.StudentErrorResponse;
+import com.yash.dev.exception.StudentNotFoundException;
 import jakarta.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,7 +42,33 @@ public class StudentRestController {
     //define endpoint for "/students/{studentId}" - retrieve the student details by studentId
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
+        if((studentId >= theStudents.size()) || (studentId <0)) {
+            throw new StudentNotFoundException("Student Id is not found - "+studentId);
+        }
         return theStudents.get(studentId);
     }
 
+    //Add an exception handler for StudentIDNotFoundException using @ExceptionHandler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+        //create a StudentErrorResponse
+        StudentErrorResponse stuErrResp=new StudentErrorResponse();
+        stuErrResp.setStatus(HttpStatus.NOT_FOUND.value());
+        stuErrResp.setMessage(exc.getMessage());
+        stuErrResp.setTimeStamp(System.currentTimeMillis());
+        //return ResponseEntity
+        return new ResponseEntity<>(stuErrResp, HttpStatus.NOT_FOUND);
+    }
+
+    //Add exception handler for otherExceptions using @ExceptionHandler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+        //create a StudentErrorResponse
+        StudentErrorResponse stuErrResp=new StudentErrorResponse();
+        stuErrResp.setStatus(HttpStatus.BAD_REQUEST.value());
+        stuErrResp.setMessage(exc.getMessage());
+        stuErrResp.setTimeStamp(System.currentTimeMillis());
+        //return ResponseEntity
+        return new ResponseEntity<>(stuErrResp, HttpStatus.BAD_REQUEST);
+    }
 }
---
+++ 01-spring-boot-rest-crud/src/main/java/com/yash/dev/exception/StudentNotFoundException.java
@@ -0,0 +1,22 @@
+package com.yash.dev.exception;
+
+/**
+ * @author yashwanthanands
+ */
+public class StudentNotFoundException extends RuntimeException {
+
+    public StudentNotFoundException() {
+    }
+
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+
+    public StudentNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StudentNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
