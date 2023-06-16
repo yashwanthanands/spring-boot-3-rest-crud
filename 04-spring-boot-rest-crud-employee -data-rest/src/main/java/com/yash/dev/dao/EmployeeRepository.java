2b16f8ba5156d55a039f42d6e515513cc5d2de94
Yashwanth Anand S <yashwanthanands@outlook.com>
2023-06-16 13:29:57 +0530

    Added Custom path for Data rest endpoints

--- 04-spring-boot-rest-crud-employee -data-rest/src/main/java/com/yash/dev/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud-employee -data-rest/src/main/java/com/yash/dev/dao/EmployeeRepository.java
@@ -2,10 +2,13 @@ package com.yash.dev.dao;
 
 import com.yash.dev.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * @author yashwanthanands
  */
+
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
