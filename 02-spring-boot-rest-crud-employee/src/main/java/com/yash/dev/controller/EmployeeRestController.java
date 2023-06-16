5c006470f9d146263351b70d2c6409f0ca43fb18
Yashwanth Anand S <yashwanthanands@outlook.com>
2023-06-16 09:43:13 +0530

    Added GetEmployee endpoint logic in controller

--- 02-spring-boot-rest-crud-employee/src/main/java/com/yash/dev/controller/EmployeeRestController.java
+++ 02-spring-boot-rest-crud-employee/src/main/java/com/yash/dev/controller/EmployeeRestController.java
@@ -5,6 +5,7 @@ import com.yash.dev.entity.Employee;
 import com.yash.dev.service.EmployeeService;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +26,16 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> getEmployees() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if(theEmployee == null) {
+            throw new RuntimeException("Employee Id not found "+employeeId);
+        }
+        return theEmployee;
     }
 
 }
