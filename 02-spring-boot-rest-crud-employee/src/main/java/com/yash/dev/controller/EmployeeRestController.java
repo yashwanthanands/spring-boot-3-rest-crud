e7efc907c6e402b0d3a781eb28185679ba047fd5
Yashwanth Anand S <yashwanthanands@outlook.com>
2023-06-16 09:52:11 +0530

    Added Create Employee Endpoint logic

--- 02-spring-boot-rest-crud-employee/src/main/java/com/yash/dev/controller/EmployeeRestController.java
+++ 02-spring-boot-rest-crud-employee/src/main/java/com/yash/dev/controller/EmployeeRestController.java
@@ -3,9 +3,12 @@ package com.yash.dev.controller;
 import com.yash.dev.dao.EmployeeDAO;
 import com.yash.dev.entity.Employee;
 import com.yash.dev.service.EmployeeService;
+import jakarta.annotation.PostConstruct;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,11 +26,13 @@ public class EmployeeRestController {
         employeeService=empService;
     }
 
+    //Add mapping for Get All Employees from DB
     @GetMapping("/employees")
     public List<Employee> getEmployees() {
         return employeeService.findAll();
     }
 
+    //Add mapping for Get Employee Details by Employee ID
     @GetMapping("/employees/{employeeId}")
     public Employee getEmployee(@PathVariable int employeeId) {
         Employee theEmployee = employeeService.findById(employeeId);
@@ -38,4 +43,11 @@ public class EmployeeRestController {
         return theEmployee;
     }
 
+    //Add mapping for post Employee Details to DB
+    @PostMapping("/employees")
+    public Employee saveEmployee(@RequestBody Employee theEmployee) {
+        theEmployee.setId(0);
+        Employee dbEmployee =employeeService.save(theEmployee);
+        return dbEmployee;
+    }
 }
